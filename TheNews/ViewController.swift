900018996b2820a2a22f353ed61f6c7f9bdffa08
MD SAZID HASAN DIP <dipcse07@gmail.com>
2020-08-29 17:38:02 +0600

    Initial Commit

---
+++ TheNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheNews
+//
+//  Created by MD SAZID HASAN DIP on 29/8/20.
+//  Copyright © 2020 MD SAZID HASAN DIP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
